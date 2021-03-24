43d801a0d7142801691ee0e58d289cb3c8b1dc92
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-03-24 14:39:47 +0100

    [ADDED] Print of the hidden word #3
    The word that is chosen randomly is printed on the screen, but hidden
    and only one line will be visible for each letter. (Temporary style)

--- assets/js/get_hidden_word.js
+++ assets/js/get_hidden_word.js
@@ -1,20 +1,38 @@
+//Given an array of individual letters, print the letters with their own style
+function printHiddenWord(list) {
+    getWordContainer().innerHTML = "";
+    for (letter of list) {
+        var singleLetterNode = document.createElement("div");
+        var singleLetterTextNode = document.createTextNode(letter);
+        singleLetterNode.appendChild(singleLetterTextNode);
+        singleLetterNode.classList.add("letter")
+        getWordContainer().appendChild(singleLetterNode)
+    }
+}
+
+//This function intercepts the word container
+function getWordContainer() {
+    let containerWord = document.getElementById("word-container");
+    return containerWord;
+}
+
 //Given a string made up of a list of words without punctuation marks,
 //it returns a random word
 function getHiddenWord(stringOfWords) {
     let hiddenWord = stringOfWords.split(" ");
     let i = randomIndex(hiddenWord);
     return hiddenWord[i].toUpperCase();
-  }
-  
-  //Given an array, it always returns a different random index
-  function randomIndex(list) {
+}
+
+//Given an array, it always returns a different random index
+function randomIndex(list) {
     let index = Math.floor(Math.random() * list.length);
     while (index == wordsListIndex) {
-      console.log()
-      index = Math.floor(Math.random() * list.length);  
+        console.log()
+        index = Math.floor(Math.random() * list.length);
     }
-      wordsListIndex = index;
-      console.log("the current index is: ", index);
-      console.log("the global index is: ", wordsListIndex);
-      return index;
-  }
+    wordsListIndex = index;
+    console.log("the current index is: ", index);
+    console.log("the global index is: ", wordsListIndex);
+    return index;
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -20,116 +20,102 @@ getButton().addEventListener("click", init);
 init();
 
 function init() {
-  
-    hiddenWord =  getHiddenWord(words);
+
+    hiddenWord = getHiddenWord(words);
     wordLetters = hiddenWord.split("");
-   
+
     console.log(words);
     console.log(hiddenWord);
     console.log(wordLetters);
-  
-  
-  
+
+    printHiddenWord(hiddenWord);
+
+
     //document.getElementsByClassName("keyboard-key").setAttribute("disabled",false);
-//   let lettersList = getWordLetters(words);
-//   printHiddenWord(lettersList);
-   
-//   wordLetters = lettersList;
+    //   let lettersList = getWordLetters(words);
+
+    //   wordLetters = lettersList;
+}
+
+//This function intercepts the refresh word button
+function getButton() {
+    return document.getElementById("btn");
 }
 
 function play(listOfLetters) {
-  console.log("Attempt Letter:", attemptLetter);
- // console.log("Right Letter:", rightLetter); 
-  letterContained(listOfLetters); 
-  if (strike < 5) {
-    
-    for (let i = 0; i<getWordContainer().childNodes.length; i++) {
-      if (getWordContainer().childNodes[i].innerHTML === attemptLetter) {
-        
-        getWordContainer().childNodes[i].classList.add("visible");
-        
-      } else {
-        console.log("Ehilà!");
-        
-      }
-      document.getElementById(attemptLetter).setAttribute("disabled", "");
+    console.log("Attempt Letter:", attemptLetter);
+    // console.log("Right Letter:", rightLetter); 
+    letterContained(listOfLetters);
+    if (strike < 5) {
+
+        for (let i = 0; i < getWordContainer().childNodes.length; i++) {
+            if (getWordContainer().childNodes[i].innerHTML === attemptLetter) {
+
+                getWordContainer().childNodes[i].classList.add("visible");
+
+            } else {
+                console.log("Ehilà!");
+
+            }
+            document.getElementById(attemptLetter).setAttribute("disabled", "");
+        }
+
     }
-    
-  }   
 }
 
 function letterContained(listOfLetters) {
-  for (let i = 0; i < listOfLetters.length; i++) {
-    if (attemptLetter === listOfLetters[i]) {
-      rightLetter = listOfLetters[i];
+    for (let i = 0; i < listOfLetters.length; i++) {
+        if (attemptLetter === listOfLetters[i]) {
+            rightLetter = listOfLetters[i];
+        }
     }
-  }
 }
 
-function printHiddenWord(list) {
-  let containerWord = document.getElementById("hidden-word-container");
-  containerWord.innerHTML = "";
-  for (letter of list) {
-    var singleLetterNode = document.createElement("div");
-    var singleLetterTextNode = document.createTextNode(letter);
-    singleLetterNode.appendChild(singleLetterTextNode);
-    singleLetterNode.classList.add("letter")
-    containerWord.appendChild(singleLetterNode)
-  } 
-}
 
-function getWordContainer() {
-  let containerWord = document.getElementById("hidden-word-container");
-  return containerWord;
-}
-
-function getButton() {
-  return document.getElementById("btn");
-}
 
 function getkeyboardContainer() {
-  return document.getElementById("keyboard-container");
+    return document.getElementById("keyboard-container");
 }
 
 getKeyboard()
 function getKeyboard() {
-  
-  for (let i = 65; i<=90; i++) {
-    
-    let keyboardKey = document.createElement("button");
-    let keyboardKeyTextNode = document.createTextNode(String.fromCharCode(i));
-    keyboardKey.appendChild(keyboardKeyTextNode);
-    keyboardKey.classList.add("keyboard-key");
-    keyboardKey.setAttribute("id", String.fromCharCode(i))
-    getkeyboardContainer().appendChild(keyboardKey);
-  }
+
+    for (let i = 65; i <= 90; i++) {
+
+        let keyboardKey = document.createElement("button");
+        let keyboardKeyTextNode = document.createTextNode(String.fromCharCode(i));
+        keyboardKey.appendChild(keyboardKeyTextNode);
+        keyboardKey.classList.add("keyboard-key");
+        keyboardKey.setAttribute("id", String.fromCharCode(i))
+        getkeyboardContainer().appendChild(keyboardKey);
+    }
 }
 
 function getaAttemptLetter(letter) {
-  attemptLetter = document.getElementById(letter).innerHTML;
+    attemptLetter = document.getElementById(letter).innerHTML;
 }
 
 document.getElementById("A").addEventListener("click", function () {
-  getaAttemptLetter("A");
-  play(wordLetters);
+    getaAttemptLetter("A");
+    play(wordLetters);
 });
 
 document.getElementById("B").addEventListener("click", function () {
-  getaAttemptLetter("B");
-  play(wordLetters);
+    getaAttemptLetter("B");
+    play(wordLetters);
 });
 
 document.getElementById("C").addEventListener("click", function () {
-  getaAttemptLetter("C");
-  play(wordLetters);
+    getaAttemptLetter("C");
+    play(wordLetters);
 });
 
 document.getElementById("D").addEventListener("click", function () {
-  getaAttemptLetter("D");
-  play(wordLetters);
+    getaAttemptLetter("D");
+    play(wordLetters);
 });
 
 document.getElementById("E").addEventListener("click", function () {
-  getaAttemptLetter("E");
-  play(wordLetters);
+    getaAttemptLetter("E");
+    play(wordLetters);
 });
