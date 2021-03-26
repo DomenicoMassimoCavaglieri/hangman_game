3c2d2cb2e9f80f9e7fe17cb35a237d2cf17af35c
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-03-26 15:29:20 +0100

    [ADDED] Hangman draw

---
+++ assets/js/manage_hangman.js
@@ -0,0 +1,28 @@
+
+function showHangPart(element) {
+    element.classList.remove("hangman-hidden");
+}
+
+function getHangHead() {
+    return document.getElementById("head");
+}
+
+function getHangBody() {
+    return document.getElementById("body");
+}
+
+function getHangArmL() {
+    return document.getElementById("arm-l");
+}
+
+function getHangArmR() {
+    return document.getElementById("arm-r");
+}
+
+function getHanglegL() {
+    return document.getElementById("leg-l");
+}
+
+function getHanglegR() {
+    return document.getElementById("leg-r");
+}
