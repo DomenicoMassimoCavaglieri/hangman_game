9a1739ad43c904b664767ba173e12d5f971dfc58
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-03-27 06:53:03 +0100

    [FIX] Modified comments to functions

--- assets/js/manage_hangman.js
+++ assets/js/manage_hangman.js
@@ -1,53 +1,19 @@
-
-
+//This function deletes all parts of the hanged man present in his container
 function hideHangPart() {
     while (getHangContainer().hasChildNodes) {
         getHangContainer().removeChild(getHangContainer().firstChild);
     }
-    
-    // for (let i= 13; i <=18; i++) {
-        
-    //     getDrawGroup().removeChild(childNodes[i]);
-    // }
 }
 
-// function showHangPart(element) {
-//     element.classList.remove("hangman-hidden");
-// }
-
-// function getHangHead() {
-//     return document.getElementById("hang-head");
-// }
-
-// function getHangBody() {
-//     return document.getElementById("hang-body");
-// }
-
-// function getHangArmL() {
-//     return document.getElementById("hang-arm-l");
-// }
-
-// function getHangArmR() {
-//     return document.getElementById("hang-arm-r");
-// }
-
-// function getHangLegL() {
-//     return document.getElementById("hang-leg-l");
-// }
-
-// function getHangLegR() {
-//     return document.getElementById("hang-leg-r");
-// }
-
-
-function getHangContainer() {
-    return document.getElementById("hangman-container");
-}
-
+//This function creates a single part of the hanged man by entering its id
 function getHangParts(idName) {
     let hangPart = document.createElement("div");
     hangPart.setAttribute("id", idName);
     hangPart.classList.add("hangman");
     getHangContainer().appendChild(hangPart);
-    
+}
+
+//This function intercepts the container of the hanged man's drawing
+function getHangContainer() {
+    return document.getElementById("hangman-container");
 }
