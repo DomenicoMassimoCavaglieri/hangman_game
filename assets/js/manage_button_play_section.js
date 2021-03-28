f147e566d1068858a0abab1814d2e35d9a5c0ecf
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-03-28 15:07:08 +0200

    [FIX] Minor bug

--- assets/js/manage_button_play_section.js
+++ assets/js/manage_button_play_section.js
@@ -24,6 +24,9 @@ function getChangeTopicButton() {
     document.getElementById("change-topic-btn").addEventListener("click", function () {
         getPopMessage("pop-box-large", "Choose the topic", popMessagesLarge); 
         setPopButtonOnListening("pop-box-large");
+        //Disabled all buttons
+        setKeyboardDisabled();
+        setPlayButtonDisabled();
     });
 }
 
