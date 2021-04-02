4da76cf6b318431d91864221e9582f154aa0ac93
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-04-02 17:12:37 +0200

    [UPDATE] Assets js #16
    Renamed the .js files to make them more readable.
    Created play_manager.js containing the game-related part, leaving only
    the game preparation part in index.js.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,4 @@
 
-//Attempts failed counter
-let strike = 4;
-
-//Game won counter
-let won = 0;
-
-//Game lost counter
-let lost = 0;
-
 //Initial Popup
 getPopMessage("pop-box-large", "Choose the topic", popMessagesLarge); 
 setPopButtonOnListening("pop-box-large");
@@ -45,58 +36,4 @@ function gamePreparation() {
     hideHangPart();
 
     printScore(won, lost);
-}
-
-/**
- * Associated with each key on the keyboard 
- * checks if the chosen letter is present
- * the score conditions and the status of the keys and "hanged man"
- */
-function play() {
-    let LettersGuessForAttempt = 0;
-    for (let i = 0; i < getLetter().length; i++) {
-        if (getLetter()[i].innerHTML === attemptLetter) {
-            getLetter()[i].classList.add("visible");
-            LettersGuessForAttempt++;
-            if (getLetterGuess().length === getLetter().length) {
-                setKeyboardDisabled();
-                setPlayButtonDisabled();
-                hideHangPart();
-                getPopMessage("pop-box-small", "You Win!", popMessageSmall);
-                setPopButtonOnListening("pop-box-small");
-                won++;
-                printScore(won, lost);
-            }
-        }
-        document.getElementById(attemptLetter).setAttribute("disabled", true);
-    }
-
-    if (LettersGuessForAttempt == 0) {
-        (strike--)
-        switch (strike) {
-            case 0: 
-                getHangParts("hang-leg-l");
-                getHangParts("hang-leg-r");
-                setKeyboardDisabled();
-                setPlayButtonDisabled();
-                setWordVisible();
-                getPopMessage("pop-box-small", "You lost...", popMessageSmall);
-                setPopButtonOnListening("pop-box-small");
-                lost++;
-                printScore(won, lost);
-                break;
-            case 1: 
-                getHangParts("hang-arm-l");
-                getHangParts("hang-arm-r");
-                break;
-            case 2:
-                getHangParts("hang-body");
-                break;
-
-            case 3:
-                getHangParts("hang-head");
-                break;
-        }
-        
-    }
 }
---
+++ assets/js/play_manager.js
@@ -0,0 +1,63 @@
+//Attempts failed counter
+let strike = 4;
+
+//Game won counter
+let won = 0;
+
+//Game lost counter
+let lost = 0;
+
+
+/**
+ * Associated with each key on the keyboard 
+ * checks if the chosen letter is present
+ * the score conditions and the status of the keys and "hanged man"
+ */
+ function play() {
+    let LettersGuessForAttempt = 0;
+    for (let i = 0; i < getLetter().length; i++) {
+        if (getLetter()[i].innerHTML === attemptLetter) {
+            getLetter()[i].classList.add("visible");
+            LettersGuessForAttempt++;
+            if (getLetterGuess().length === getLetter().length) {
+                setKeyboardDisabled();
+                setPlayButtonDisabled();
+                hideHangPart();
+                getPopMessage("pop-box-small", "You Win!", popMessageSmall);
+                setPopButtonOnListening("pop-box-small");
+                won++;
+                printScore(won, lost);
+            }
+        }
+        document.getElementById(attemptLetter).setAttribute("disabled", true);
+    }
+
+    if (LettersGuessForAttempt == 0) {
+        (strike--)
+        switch (strike) {
+            case 0: 
+                getHangParts("hang-leg-l");
+                getHangParts("hang-leg-r");
+                setKeyboardDisabled();
+                setPlayButtonDisabled();
+                setWordVisible();
+                getPopMessage("pop-box-small", "You lost...", popMessageSmall);
+                setPopButtonOnListening("pop-box-small");
+                lost++;
+                printScore(won, lost);
+                break;
+            case 1: 
+                getHangParts("hang-arm-l");
+                getHangParts("hang-arm-r");
+                break;
+            case 2:
+                getHangParts("hang-body");
+                break;
+
+            case 3:
+                getHangParts("hang-head");
+                break;
+        }
+        
+    }
+}
